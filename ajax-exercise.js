e6f780a61c69c87cf0af21a09a72dc38ce49672e
Caitlin Baldini <cat@listoglobal.com>
2024-01-25 19:55:49 -0700

    progress

--- ajax-exercise.js
+++ ajax-exercise.js
@@ -1,39 +1,65 @@
-import axios from 'axios';
-
-// PART 1: Show Dog Photo
+import axios from "axios";
 
 function showDogPhoto(evt) {
-  // TODO: get a random photo from the Dog API and show it in the #dog-image div
+  axios.get("https://dog.ceo/api/breeds/image/random").then((res) => {
+    const imgUrl = res.data.message;
+    document.querySelector("#dog-image").innerHTML = `<img src=${imgUrl}`;
+  });
 }
 
-document.querySelector('#get-dog-image').addEventListener('click', showDogPhoto);
-
-// PART 2: Show Weather
+document
+  .querySelector("#get-dog-image")
+  .addEventListener("click", showDogPhoto);
 
 function showWeather(evt) {
-  const zipcode = document.querySelector('#zipcode-field').value;
+  const zipcode = document.querySelector("#zipcode-field").value;
 
-  // TODO: request weather with that URL and show the forecast in #weather-info
+  axios.get(`/weather.txt?zipcode=${zipcode}`).then((res) => {
+    const zipUrl = res.data.forecast;
+    document.querySelector("#weather-info").innerHTML = res.data;
+  });
 }
 
-document.querySelector('#weather-button').addEventListener('click', showWeather);
+document
+  .querySelector("#weather-button")
+  .addEventListener("click", showWeather);
 
-// PART 3: Order Cookies
-
-function orderCookies(evt) {
-  // TODO: Need to preventDefault here, because we're listening for a submit event!
-  // TODO: show the result message after your form
-  // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+async function orderCookies(evt) {
+  evt.preventDefault();
+  const cookieType = document.querySelector("#cookie-type-field");
+  const qty = document.querySelector("#qty-field");
+  const response = await axios.post("/order-cookies.jason", {
+    cookieType: cookieType,
+    qty: qty,
+  });
+  const orderStatus = document.querySelector("#order-status");
+  orderStatus.innerText = res.data.message;
+  if (res.data.resultCode === "ERROR") {
+    orderStatus.classList.add("order-error");
+  } else {
+    orderStatus.classList.remove("order-error");
+  }
 }
-document.querySelector('#order-form').addEventListener('submit', orderCookies);
+document.querySelector("#order-form").addEventListener("submit", orderCookies);
 
 // PART 4: iTunes Search
 
-function iTunesSearch(evt) {
+async function iTunesSearch(evt) {
   evt.preventDefault();
   const searchTerm = document.querySelector("#search-term").value;
 
-  // TODO: In the #itunes-results list, show all results in the following format:
-  // `Artist: ${artistName} Song: ${trackName}`
+  const formData = { term: searchTerm };
+  const queryString = new URLSearchParams(formData).toString();
+  const url = `http://itunes.apple.com/search/${queryString}`;
+
+  const response = await axios.get(url);
+  let displayString = "";
+  for (const result of response.data.results) {
+    displayString += `<li> Artist: ${result.artistName} Song: ${result.trackName}</li>`;
+    document.querySelector("itunes-results").innerHTML = displayString;
+  }
 }
-document.querySelector('#itunes-search-form').addEventListener('submit', iTunesSearch);
+
+document
+  .querySelector("#itunes-search-form")
+  .addEventListener("submit", iTunesSearch);
